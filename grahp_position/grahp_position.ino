97e42752f0133661711e581f035e2cdd296e2e3e
Silvak <silvartistt@gmail.com>
2018-06-09 01:47:41 -0400

    Correcion..

--- grahp_position/grahp_position.ino
+++ grahp_position/grahp_position.ino
@@ -5,14 +5,14 @@ int  x = 0; //Almacena el xPin
 int  y = 0; //Almacena el yPin
 int gx = 0; //Almacena el resultado de la division entre x / 10
 int gy = 0; //Almacena el resultado de la division entre y / 10
-int mode = 0; //un valor a ser untilizado posteriormente para un cambio de codigo al pulsar un boton
+int mode = 0; //un valor a ser untilizado posteriormente para un cambio de modos al pulsar un boton
 
 int DIN = 12; 
 int CS =  11;
 int CLK = 10;
 
-byte  px[]=  {0x80,0x40,0x20,0x10,0x08,0x04,0x02,0x01}; //punto de posicion
-byte  py[8]=  {0,0,0,0,0,0,0,0}; //punto de posicion en blanco
+byte  px[]=   {0x80,0x40,0x20,0x10,0x08,0x04,0x02,0x01}; //Puntos de posicion en el eje X
+byte  py[8]=  {0,0,0,0,0,0,0,0}; //Puntos de posicion en el eje Y
 //Animacion inicial a0, a1 y a2
 byte a0[8]=  {0,0,0,0,0,0,0,0}; 
 byte a1[8]=  {0x00,0x00,0x00,0x18,0x18,0x08,0x00,0x00};
@@ -23,10 +23,10 @@ LedControl lc=LedControl (DIN,CLK,CS,0);
 void setup() {
   Serial.begin(9600);
   analogReference(EXTERNAL);
-  pinMode(xPin, INPUT);       // establecemos xPin como variable de entrada
-  pinMode(yPin, INPUT);       // establecemos yPin como variable de entrada
-  lc.shutdown(0,false);       //The MAX72XX is in power-saving mode on startup
-  lc.setIntensity(0, 5);      // Configuracion del brillo para max o min valor
+  pinMode(xPin, INPUT);       // establece xPin como variable de entrada
+  pinMode(yPin, INPUT);       // establece yPin como variable de entrada
+  lc.shutdown(0,false);       // El MAX72XX is in power-saving mode on startup
+  lc.setIntensity(0, 5);      // Configuracion del brillo para max o min valor 5 - 30...
   lc.clearDisplay(0);         // Limpiador de pantalla
   Serial.println("Wave_arduino grafica un punto en matriz 8x8 con acelerometro"); delay(100);
   Serial.println("               Recordar leer todo por anticipado            "); delay(900);
@@ -35,7 +35,7 @@ void setup() {
 void loop() {
   //Llamado a la funcion logo y comprobacion si se ha ejecutado con anterioridad
   if(mode == 0) { Logo(); } 
-  //Llamamos a la funcion para graficar el punto en la matriz
+  //Llamado a la funcion para graficar el punto en la matriz
   if(mode == 1) { GraficaPoint(); }
 }
 
@@ -67,17 +67,18 @@ void GraficaPoint(){
     //leemos y almacenamos la posicion en x & y  
     x = analogRead(xPin) - 501; 
     y = analogRead(yPin) - 510; 
-    //la dividimos entre dies para obtener enteros de menor valor
+    //la dividimos entre diez para obtener enteros de menor valor
     gx = x / 10;
     Serial.print(gx);
     Serial.print(":x\t y:");
     gy = y / 10;
     Serial.println(gy);
-    if(gy >= 0){  //definimos que gy > 0 ejecutar...
-        /*la pocicion gy definira dinamicamente el punto a graficar y, mientras que gx tomara un valor*/
+    if(gy >= 0){  //Si gy >= 0 ejecutar...
+        /*la pocicion Y esta definida dinamicamente por gy utilizando los valores del 0 al 7, 
+        mientras que gx toma un valor de la matriz px utilizando 0 or 7 para graficar la posicion en X*/
         py[gy] = px[gx]; 
-        printByte(py);
-        py[gy]={0x00};
+        printByte(py); //Se ejecuta la funcion printByte(namematriz); para imprimir el resultado
+        py[gy]= 0; //Restablece la posicion en el eje Y a su valor inicial
         }
 }
 
